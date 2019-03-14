8daf5f6094c6e50b1a3e01f76da224af360c81f0
Shahram Barai <shahram.barai@aalto.fi>
2019-03-14 12:16:55 +0200

    Add square.py

---
+++ square.py
@@ -0,0 +1,10 @@
+class Square():
+    
+    def __init__(self, is_wall=False):
+        self.is_wall = is_wall
+         
+    def is_wall_square(self):
+        return self.is_wall
+    
+    def set_wall(self):
+        self.is_wall = True
