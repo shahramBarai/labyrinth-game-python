d4ac2dde5d6015a74df2a902fc6f0624545fb6af
Shahram Barai <shahram.barai@aalto.fi>
2019-03-28 11:17:46 +0200

    Apdated

--- src/gui.py
+++ src/gui.py
@@ -7,13 +7,15 @@ from player_graphics_item import PlayerGraphicsItem
 
 class GUI(QtWidgets.QMainWindow):
     
-    def __init__(self, world, square_size):
+    def __init__(self, world):
         super().__init__()
         self.setCentralWidget(QtWidgets.QWidget())
         self.grid = QtWidgets.QGridLayout(self)
         self.centralWidget().setLayout(self.grid)
         self.world = world
-        self.square_size = square_size
+        self.square_size = self.world.get_square_size()
+        self.gridItems = []
+        self.game_started = 0
         
         self.init_window()
         self.init_buttons()
@@ -25,18 +27,21 @@ class GUI(QtWidgets.QMainWindow):
         # Set a timer to call the update function periodically
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_player)
-        self.timer.start(2) # Milliseconds
+        self.timer.start(10) # Milliseconds
     
     def add_labyrinth_world_grid_items(self):
-        for y in range(self.world.get_height()):
-            for x in range(self.world.get_width()):
-                #Piirretaan suorakulmio
-                SquareItem = QtWidgets.QGraphicsRectItem(x * self.square_size, y * self.square_size, self.square_size, self.square_size)
-                if self.world.get_square(x, y).is_wall_square():
-                    SquareItem.setBrush(QtGui.QColor(0,0,0))
-                else: SquareItem.setBrush(QtGui.QColor(220,240,220))
-                    
-                self.scene.addItem(SquareItem)
+        #Draw a game zone
+        mapzoneItem = QtWidgets.QGraphicsRectItem(0, 0, self.square_size*self.world.get_width(), self.square_size*self.world.get_height())
+        mapzoneItem.setBrush(QtGui.QColor(220,240,220))
+        self.scene.addItem(mapzoneItem)
+        for x in range(self.world.get_width()):
+            for y in range(self.world.get_height()):
+                # Draw the walls
+                squareItem = self.world.get_square(x, y)
+                self.scene.addItem(squareItem)
+              
+    def update_grid_item(self, x, y):
+        self.world.get_square(x, y).update()
     
     def add_player_graphics_item(self):
         self.playerItem = PlayerGraphicsItem(self.world.get_player(), self.square_size)
@@ -46,24 +51,24 @@ class GUI(QtWidgets.QMainWindow):
     def keyPressEvent(self, event):
         key = event.key()
         if key == QtCore.Qt.Key_W:
-            self.world.get_player().try_to_move(0, -1)
+            self.world.get_player().try_to_move("UP")
         elif key == QtCore.Qt.Key_S:
-            self.world.get_player().try_to_move(0, 1)
+            self.world.get_player().try_to_move("DOWN")
         elif key == QtCore.Qt.Key_A:
-            self.world.get_player().try_to_move(-1, 0)
+            self.world.get_player().try_to_move("LEFT")
         elif key == QtCore.Qt.Key_D:
-            self.world.get_player().try_to_move(1, 0)
+            self.world.get_player().try_to_move("RIGHT")
             
     def keyReleaseEvent(self, event):
         key = event.key()
         if key == QtCore.Qt.Key_Up:
-            self.world.get_player().try_to_move(0, -1)
+            self.world.get_player().try_to_move("UP")
         elif key == QtCore.Qt.Key_Down:
-            self.world.get_player().try_to_move(0, 1)
+            self.world.get_player().try_to_move("DOWN")
         elif key == QtCore.Qt.Key_Left:
-            self.world.get_player().try_to_move(-1, 0)
+            self.world.get_player().try_to_move("LEFT")
         elif key == QtCore.Qt.Key_Right:
-            self.world.get_player().try_to_move(1, 0)
+            self.world.get_player().try_to_move("RIGHT")
          
     def init_buttons(self):
         self.new_game_button = QtWidgets.QPushButton("New Game")
@@ -74,8 +79,9 @@ class GUI(QtWidgets.QMainWindow):
     
         
     def update_player(self):
+        if self.game_started == 0:
+            self.game_started = self.world.create_maze_randomly()
         self.playerItem.update()
-                   
            
     def init_window(self):  
         # Sets up the window.
@@ -89,6 +95,7 @@ class GUI(QtWidgets.QMainWindow):
         # Add a view for showing the scene
         self.view = QtWidgets.QGraphicsView(self.scene, self)
         self.view.adjustSize()
+        self.view.setBackgroundBrush(QtGui.QColor(0, 0, 0))
         self.view.show()
             
         self.grid.setColumnStretch(0, 8)
