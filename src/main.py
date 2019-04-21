9d772d08ea9fd65854f20ec58ec3795f1dbcc597
Shahram Barai <shahram.barai@aalto.fi>
2019-04-21 23:28:42 +0300

    Small changes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMessageBox, QSpacerItem, QSizePolicy
+from PyQt5.QtWidgets import QApplication
 
 from gui import GUI
 from labyrinthworld import LabyrinthWorld
-from player import Player
-from coordinates import Coordinates
 
 def main():
     
     test_world = LabyrinthWorld()
-    test_world.create_grid(49, 49, 10)
-    test_world.set_player(Player("Name_example", test_world))
-    test_world.get_player().set_location(Coordinates(0,0))
 
     app = QApplication(sys.argv)
-    #app.setStyle("Fusion")
+    app.setStyle("Fusion")
     gui = GUI(test_world)
 
     sys.exit(app.exec_())
